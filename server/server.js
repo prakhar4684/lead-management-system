d6a39809864f87cf23a9925e208c629358308b36
Prakhar Shukla <praakharshukla@4004gmail.com>
2026-07-26 01:47:39 +0530

    Update production API URL

--- server/server.js
+++ server/server.js
@@ -9,11 +9,12 @@ const cors=require('cors');
 env.config();
 
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true
-}));
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
